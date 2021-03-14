dc643a713bf57ed36ae295e8ca2d256b4d10c9e8
Markoborodov <aamark99@gmail.com>
2021-03-14 13:25:53 +0300

    Add commands to the buttons
    - For "Exit": close the window
    - For "New": reorder the tiles
    - For tiles: move if the empty space is near and check if it is the winning order

--- 03_ThreeWayAndTkinter/main.py
+++ 03_ThreeWayAndTkinter/main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import tkinter.messagebox as mb
+from random import shuffle as rm_shuffle
 
 def create_window():
     window = tk.Tk()
@@ -11,14 +13,38 @@ def create_window():
     return window
 
 
+def new_tiles_order(tiles, order):
+    rm_shuffle(order)
+    order[order.index(15)] = order[-1]
+    order[-1] = 15
+    for i, tile in enumerate(tiles):
+        tile.grid(column=order[i]%4, row=order[i]//4+1, sticky=tk.E+tk.W+tk.N+tk.S)
+
+
+def move_tile(i, order, tiles):
+    tile_column, tile_row = tiles[i].grid_info()['column'], tiles[i].grid_info()['row']
+    space_column, space_row = order[-1] % 4, order[-1] // 4 + 1
+    if (tile_column == space_column and abs(tile_row - space_row) == 1
+        or tile_row == space_row and abs(tile_column - space_column) == 1):
+        tiles[i].grid(column=space_column, row=space_row)
+        order[i], order[-1] = order[-1], order[i]
+        if order == list(range(16)):
+            mb.showinfo('','You win!')
+            new_tiles_order(tiles, order)
+
+
 def create_buttons(window):
-    new_button = tk.Button(window, text='New', width=10, height=2)
-    new_button.grid(column=0, row=0, columnspan=2)
-    exit_button = tk.Button(window, text='Exit', width=10, height=2)
+    exit_button = tk.Button(window, text='Exit', width=10, height=2,
+                            command=window.destroy)
     exit_button.grid(column=2, row=0, columnspan=2)
     tiles = [tk.Button(window, text=i) for i in range(1, 16)]
+    order = list(range(16))
+    new_tiles_order(tiles, order)
     for i, tile in enumerate(tiles):
-        tile.grid(column=i%4, row=i//4+1, sticky=tk.E+tk.W+tk.N+tk.S)
+        tile.configure(command=lambda i=i: move_tile(i, order, tiles))
+    new_button = tk.Button(window, text='New', width=10, height=2,
+                           command=lambda: new_tiles_order(tiles, order))
+    new_button.grid(column=0, row=0, columnspan=2)
 
 
 window = create_window()
